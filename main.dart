14721f84d89e84e608957e0511286c49720ee693
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-11-06 13:18:15 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+  /*
+  print("Question no 1:");
+  List mylist = ["madiha", "huzaifa", "abdullah", "mahira", "maaz"];
+  print(mylist);
+  print("Question no 02");
+  List<String> days = [];
+  days.addAll([
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ]);
+  print(days);*/
+}
